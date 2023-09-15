544318cd5afb63e06f9a3a2e35574d36fc9412d6
Robin Long <binlon@amazon.com>
2023-09-15 17:28:18 +0800

    update startSageCPU.sh

---
+++ startSageCPU.sh
@@ -0,0 +1,2 @@
+cd ~/SageMaker/awesome/do/sd-aws-extension/stable-diffusion-webui
+./webui.sh --enable-insecure-extension-access --port 8760 --allow-code --medvram --xformers --subpath proxy/8760/ --listen
